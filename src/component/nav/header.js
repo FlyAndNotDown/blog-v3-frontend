69ef19b8b4296de96f62e13c45dd285a297ccc9c
John Kindem <461425614@qq.com>
2018-07-28 02:30:39 +0800

    [del] 删库跑路，还是想写带 Header 的博客页面，不想做的太简单了

--- src/component/nav/header.js
+++ src/component/nav/header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { Row, Col, Avatar, Divider } from 'antd';
-import avatarImg from '../../img/avatar.jpg';
 
 /**
  * 导航
@@ -18,22 +16,7 @@ export class Header extends React.Component {
     render() {
 
         return (
-            <Row
-                className={'mt-60px'}>
-                <Col>
-                    <div className={'text-align-center'}>
-                        <Avatar
-                            className={'w-100px h-100px shadow-little'}
-                            src={avatarImg}/>
-                    </div>
-                    <h1 className={'text-align-center mb-0'}>
-                        John Kindem
-                    </h1>
-                    <h2 className={'color-second text-align-center'}>
-                        简单、快乐的前端狗
-                    </h2>
-                </Col>
-            </Row>
+            null
         );
 
     }
--- src/page/index.js
+++ src/page/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Row, Col } from 'antd';
-import { Header } from "../component/nav/header";
-import { BgColorNoneLayout } from "../component/layout/bg-color-none-layout";
-import { Nav } from "../component/nav/nav";
 
 /**
  * 页面组件 - /
@@ -12,33 +8,7 @@ export class IndexPage extends React.Component {
     render() {
 
         return (
-            <BgColorNoneLayout>
-                <Row>
-                    <Col
-                        xs={{
-                            span: 0,
-                            offset: 0
-                        }}
-                        sm={{
-                            span: 0,
-                            offset: 0
-                        }}
-                        md={{
-                            span: 0,
-                            offset: 0
-                        }}
-                        lg={{
-                            span: 16,
-                            offset: 4
-                        }}
-                        xl={{
-                            span: 12,
-                            offset: 6
-                        }}>
-                        <Header/>
-                    </Col>
-                </Row>
-            </BgColorNoneLayout>
+            null
         );
 
     }
