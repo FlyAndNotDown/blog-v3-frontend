7aad88e161f9f0b1f6294dd50cb8cd2d46518aa9
John Kindem <461425614@qq.com>
2018-08-18 23:25:39 +0800

    [add] 修改一些注释

--- src/css/css.js
+++ src/css/css.js
@@ -1,2 +1,5 @@
+// 导入全局 css
+import './main.css';
+
 // 导入高亮 css
 import './highlight/one-light.css';
---
+++ src/page/index.js
@@ -0,0 +1,35 @@
+// 导入 React 框架核心
+import React from 'react';
+
+/**
+ * IndexPage
+ */
+export class IndexPage extends React.Component {
+
+    /**
+     * 构造
+     * @param props 属性
+     */
+    constructor(props) {
+
+        // 调用父类构造
+        super(props);
+
+        // 设置组件初始状态
+        this.state = {};
+
+    }
+
+    /**
+     * 渲染函数
+     * @returns {*} jsx
+     */
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+
+}
