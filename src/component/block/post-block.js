94fd5a8e5d26b61cf7bd30550dfc355b1e322d67
John Kindem <461425614@qq.com>
2018-09-26 00:22:32 +0800

    [add] 封装 Post

--- src/component/block/post-block.js
+++ src/component/block/post-block.js
@@ -9,6 +9,14 @@ import { Link } from 'react-router-dom';
 
 /**
  * 文章块
+ * @props {number} key 文章键
+ * @props {string} title 标题
+ * @props {string} description 描述
+ * @props {string} date 日期
+ * @props {array} labels 标签
+ *  @member {object} label 标签对象
+ *      @member {string} name 标签名
+ *      @member {string} key 标签键
  */
 export class PostBlock extends React.Component {
 
@@ -30,26 +38,33 @@ export class PostBlock extends React.Component {
         return (
             <Row className={'mt-md'}>
                 <Col>
-                    <Link className={'font-size-lg color-black'} to={'#'}>测试标题</Link>
-                    <div className={'font-size-xs'}>
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
-                        测试概述测试概述测试概述测试概述测试概述
+                    <Link className={'font-size-lg color-black'} to={`${this.props.key}`}>
+                        {this.props.title}
+                    </Link>
+                    <div className={'font-size-xs mt-xs'}>
+                        {this.props.description}
                     </div>
                     <div>
                         <span className={'font-size-xs mt-sm float-left'}>
                             <Icon type={'clock-circle-o'}/>
-                            <span className={'pl-xs pr-xs'}>2018-9-21</span>
+                            <span className={'pl-xs pr-xs'}>
+                                {this.props.date}
+                            </span>
                         </span>
                             <span className={'font-size-xs mt-sm float-right'}>
-                            <Link to={'#'} className={'color-text'}>#标签</Link>&nbsp;
-                                <Link to={'#'} className={'color-text'}>#标签</Link>&nbsp;
+                            {this.props.labels.map((label, key) => {
+                                return (
+                                    <Link
+                                        to={`/label/${label.key}`}
+                                        key={key}
+                                        className={'color-text'}>
+                                        #{label.name}
+                                        {key === this.props.labels.length - 1 ? (
+                                            <span>&nbsp;</span>
+                                        ) : null}
+                                    </Link>
+                                );
+                            })}
                         </span>
                     </div>
                 </Col>
