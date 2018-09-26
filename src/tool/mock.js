3d05b5c653ae249cb57c59b4fe666b5b06581199
John Kindem <461425614@qq.com>
2018-09-26 13:52:11 +0800

    [add] 添加 Mock 数据

--- src/tool/mock.js
+++ src/tool/mock.js
@@ -29,6 +29,78 @@ export class MockTool {
             type: 'emotion',
             context: '今天真是开心的一天呢',
             date: '2018-9-26'
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
+        }, {
+            type: 'post',
+            title: '测试标题',
+            description: '测试描述测试描述测试描述测试描述测试描述测试描述',
+            date: '2018-9-26',
+            labels: [{
+                name: '标签',
+                key: 1
+            }]
         }]);
     }
 
