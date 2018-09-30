6c90506aad2399b080933114598f760d9f13219d
John Kindem <461425614@qq.com>
2018-09-30 19:04:08 +0800

    [add] 添加管理员总览页面页头

--- src/page/admin/general.js
+++ src/page/admin/general.js
@@ -5,6 +5,7 @@
 
 import React from 'react';
 import { KLayout } from '../../component/tool/k-layout';
+import { Row, Col, Divider } from 'antd';
 
 export class AdminGeneralPage extends React.Component {
 
@@ -25,7 +26,19 @@ export class AdminGeneralPage extends React.Component {
     render() {
         return (
             <KLayout>
-                
+                <Row className={'mt-md'}>
+                    <Col
+                        xs={{ offset: 1, span: 22 }}
+                        sm={{ offset: 1, span: 22 }}
+                        md={{ offset: 2, span: 20 }}
+                        lg={{ offset: 4, span: 16 }}
+                        xl={{ offset: 4, span: 16 }}
+                        xxl={{ offset: 4, span: 16 }}>
+                        <div className={'color-black font-size-lg'}>总览</div>
+                        <div className={'color-second font-size-sm'}>欢迎您 Kindem</div>
+                        <Divider/>
+                    </Col>
+                </Row>
             </KLayout>
         );
     }
