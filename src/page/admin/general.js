f69b9f1f14d843fc6f786a68b2eb057270183f6e
John Kindem <461425614@qq.com>
2018-09-30 23:24:52 +0800

    [add] 添加管理员 UI

--- src/page/admin/general.js
+++ src/page/admin/general.js
@@ -57,13 +57,169 @@ export class AdminGeneralPage extends React.Component {
                         sm={{ offset: 0, span: 0 }}
                         md={{ offset: 0, span: 0 }}
                         lg={{ offset: 4, span: 16 }}
-                        xl={{ offset: 4, span: 16 }}
-                        xxl={{ offset: 4, span: 16 }}>
-                        <div className={'font-size-xxl'}>
-                            <Link className={'color-black text-decoration-none'} to={'#'}>总览</Link>
+                        xl={{ offset: 5, span: 14 }}
+                        xxl={{ offset: 6, span: 12 }}>
+                        <div>
+                            <div className={'font-size-xxl'}>
+                                <Link className={'color-black text-decoration-none'} to={'#'}>总览</Link>
+                            </div>
+                            <div className={'color-second font-size-sm'}>
+                                <span role={'img'} aria-labelledby={'happy-face'}>😀</span>
+                                欢迎您 管理员
+                            </div>
+                            <Divider/>
                         </div>
-                        <div className={'color-second font-size-sm'}>欢迎您 管理员</div>
-                        <Divider/>
+
+                        <div>
+                            <div className={'font-size-sm'}>
+                                <Link className={'color-black text-decoration-none'} to={'#'}>今日概况</Link>
+                            </div>
+                            <Row className={'mt-lg'}>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'line-chart'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            今日点击量
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            1000
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'dot-chart'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            今日访问量
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            1000
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'message'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            今日评论
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            1000
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Divider/>
+                        </div>
+
+                        <div>
+                            <div className={'font-size-sm'}>
+                                <Link className={'color-black text-decoration-none'} to={'#'}>勤奋指数</Link>
+                            </div>
+                            <Row className={'mt-lg'}>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'file-done'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            本月博文数
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            5
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'smile'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            本月说说数
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            5
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'star'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            今年作品数
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            5
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Divider/>
+                        </div>
+
+                        <div>
+                            <div className={'font-size-sm'}>
+                                <Link className={'color-black text-decoration-none'} to={'#'}>安全指数</Link>
+                            </div>
+                            <Row className={'mt-lg'}>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'login'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            管理员账户数量
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            5
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'clock-circle-o'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            上次登录
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            2018-9-30
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col span={8}>
+                                    <div>
+                                        <div className={'text-align-center font-size-xxl color-second'}>
+                                            <Icon type={'warning'}/>
+                                        </div>
+                                        <div className={'text-align-center font-size-sm color-second'}>
+                                            异地登录
+                                        </div>
+                                        <div className={'text-align-center font-size-xs color-second'}>
+                                            无
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                            <Divider/>
+                        </div>
+
+                        <div>
+                            <div className={'font-size-sm'}>
+                                <Link className={'color-black text-decoration-none'} to={'#'}>数据库管理</Link>
+                            </div>
+                        </div>
+                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
                     </Col>
                 </Row>
             </KLayout>
