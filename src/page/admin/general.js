890d49d4c90f6504f8bfa998fa953c146ef4753c
John Kindem <461425614@qq.com>
2018-09-30 21:17:35 +0800

    [add] 添加管理员响应式

--- src/page/admin/general.js
+++ src/page/admin/general.js
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import { KLayout } from '../../component/tool/k-layout';
-import { Row, Col, Divider } from 'antd';
+import { Row, Col, Divider, Icon } from 'antd';
 import { Link } from 'react-router-dom';
 
 export class AdminGeneralPage extends React.Component {
@@ -27,15 +27,39 @@ export class AdminGeneralPage extends React.Component {
     render() {
         return (
             <KLayout>
-                <Row className={'mt-md'}>
+                <Row>
                     <Col
-                        xs={{ offset: 1, span: 22 }}
-                        sm={{ offset: 1, span: 22 }}
-                        md={{ offset: 2, span: 20 }}
+                        className={'w-100 h-100vh'}
+                        xs={{ offset: 0, span: 24 }}
+                        sm={{ offset: 0, span: 24 }}
+                        md={{ offset: 0, span: 24 }}
+                        lg={{ offset: 0, span: 0 }}
+                        xl={{ offset: 0, span: 0 }}
+                        xxl={{ offset: 0, span: 0 }}>
+                        <Row
+                            className={'w-100 h-100'}
+                            type={'flex'}
+                            align={'middle'}
+                            justify={'center'}>
+                            <Col>
+                                <div className={'text-align-center font-size-xxl'}>
+                                    <Icon type={'chrome'}/>
+                                </div>
+                                <div className={'color-second font-size-md text-align-center'}>
+                                    请在PC端登录
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col
+                        className={'mt-md'}
+                        xs={{ offset: 0, span: 0 }}
+                        sm={{ offset: 0, span: 0 }}
+                        md={{ offset: 0, span: 0 }}
                         lg={{ offset: 4, span: 16 }}
                         xl={{ offset: 4, span: 16 }}
                         xxl={{ offset: 4, span: 16 }}>
-                        <div className={'font-size-lg'}>
+                        <div className={'font-size-xxl'}>
                             <Link className={'color-black text-decoration-none'} to={'#'}>总览</Link>
                         </div>
                         <div className={'color-second font-size-sm'}>欢迎您 管理员</div>
