25509592d15f0323c9d8cbbc0c7ac5f5d77a7c4b
John Kindem <461425614@qq.com>
2018-10-01 23:45:32 +0800

    [add] 添加 404 页面

--- src/page/error-404.js
+++ src/page/error-404.js
@@ -4,6 +4,9 @@
  */
 
 import React from 'react';
+import { KLayout } from '../component/tool/k-layout';
+import { Row, Col, Icon } from 'antd';
+import { Link } from 'react-router-dom';
 
 /**
  * 404页面组件
@@ -25,9 +28,27 @@ export class Error404Page extends React.Component {
      */
     render() {
         return (
-            <div>
-
-            </div>
+            <KLayout
+                className={'w-100 h-100vh'}
+                colorMode={KLayout.COLOR_MODE_MAIN}>
+                <Row
+                    className={'w-100 h-100'}
+                    type={'flex'}
+                    align={'middle'}
+                    justify={'center'}>
+                    <Col>
+                        <div className={'text-align-center color-second font-size-xxl'}>
+                            <Icon type={'frown'}/>
+                        </div>
+                        <div className={'text-align-center color-black font-size-md'}>
+                            哎呀 页面消失了
+                        </div>
+                        <div className={'text-align-center font-size-sm'}>
+                            <Link to={'/'}>跟我一起回主站吧</Link>
+                        </div>
+                    </Col>
+                </Row>
+            </KLayout>
         );
     }
 
