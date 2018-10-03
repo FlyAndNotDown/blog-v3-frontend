480263d164b74caf9b5dfe8ba48e25c966d74414
John Kindem <461425614@qq.com>
2018-10-03 23:46:48 +0800

    [refactor] 重构一大批管理员功能

--- src/page/admin/general.js
+++ src/page/admin/general.js
@@ -35,13 +35,204 @@ export class AdminGeneralPage extends React.Component {
      */
     onWritePostButtonClick = (e) => {
         this.props.history.push('/admin/post/new');
-    }
+    };
 
     /**
      * 渲染函数
      * @return {JSX} 渲染结果
      */
     render() {
+        // 欢迎块
+        let welcomeBlock = (
+            <div>
+                <div className={'font-size-xxl'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>总览</Link>
+                </div>
+                <div className={'color-second font-size-sm'}>
+                    <span role={'img'} aria-labelledby={'happy-face'}>😀</span>
+                    欢迎您 管理员
+                </div>
+                <Divider/>
+            </div>
+        );
+        // 今日概况块
+        let todaySummaryBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>今日概况</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'line-chart'}
+                            title={'今日点击量'}
+                            content={1000}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'dot-chart'}
+                            title={'今日访问量'}
+                            content={1000}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'message'}
+                            title={'今日评论'}
+                            content={1000}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+        // 勤奋指数块
+        let hardWorkValueBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>勤奋指数</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'file-done'}
+                            title={'本月博文数'}
+                            content={5}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'smile'}
+                            title={'本月说说数'}
+                            content={5}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'star'}
+                            title={'今年作品数'}
+                            content={5}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+        // 安全指数块
+        let securityValueBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>安全指数</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'login'}
+                            title={'管理员账户数量'}
+                            content={5}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'clock-circle-o'}
+                            title={'上次登录'}
+                            content={'2018-9-30'}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'warning'}
+                            title={'异地登录'}
+                            content={'无'}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+        // 文章管理块
+        let postManageBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>文章管理</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'plus-circle'}
+                            iconColor={'#52c41a'}
+                            title={'写文章'}
+                            content={
+                                <Button
+                                    type={'primary'}
+                                    onClick={this.onWritePostButtonClick}>
+                                    启动
+                                </Button>
+                            }/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'file-text'}
+                            title={'管理文章'}
+                            content={<Button>启动</Button>}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+        // 评论管理块
+        let commentManageBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>评论管理</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'eye'}
+                            iconColor={'#1890ff'}
+                            title={'审核评论'}
+                            content={<Button type={'primary'}>启动</Button>}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'search'}
+                            title={'未审核评论'}
+                            content={12}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'message'}
+                            title={'管理评论'}
+                            content={<Button>启动</Button>}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+        // 留言管理块
+        let messageManageBlock = (
+            <div>
+                <div className={'font-size-sm'}>
+                    <Link className={'color-black text-decoration-none'} to={'#'}>留言管理</Link>
+                </div>
+                <Row className={'mt-lg'}>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'eye'}
+                            iconColor={'#1890ff'}
+                            title={'审核留言'}
+                            content={<Button type={'primary'}>启动</Button>}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'search'}
+                            title={'未审核留言'}
+                            content={12}/>
+                    </Col>
+                    <Col span={8}>
+                        <AdminIconGadget
+                            iconType={'message'}
+                            title={'管理留言'}
+                            content={<Button>启动</Button>}/>
+                    </Col>
+                </Row>
+                <Divider/>
+            </div>
+        );
+
         return (
             <KLayout>
                 <Row>
@@ -71,183 +262,13 @@ export class AdminGeneralPage extends React.Component {
                         lg={{ offset: 4, span: 16 }}
                         xl={{ offset: 5, span: 14 }}
                         xxl={{ offset: 6, span: 12 }}>
-                        <div>
-                            <div className={'font-size-xxl'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>总览</Link>
-                            </div>
-                            <div className={'color-second font-size-sm'}>
-                                <span role={'img'} aria-labelledby={'happy-face'}>😀</span>
-                                欢迎您 管理员
-                            </div>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>今日概况</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'line-chart'}
-                                        title={'今日点击量'}
-                                        content={1000}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'dot-chart'}
-                                        title={'今日访问量'}
-                                        content={1000}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'message'}
-                                        title={'今日评论'}
-                                        content={1000}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>勤奋指数</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'file-done'}
-                                        title={'本月博文数'}
-                                        content={5}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'smile'}
-                                        title={'本月说说数'}
-                                        content={5}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'star'}
-                                        title={'今年作品数'}
-                                        content={5}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>安全指数</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'login'}
-                                        title={'管理员账户数量'}
-                                        content={5}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'clock-circle-o'}
-                                        title={'上次登录'}
-                                        content={'2018-9-30'}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'warning'}
-                                        title={'异地登录'}
-                                        content={'无'}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>文章管理</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'plus-circle'}
-                                        iconColor={'#52c41a'}
-                                        title={'写文章'}
-                                        content={
-                                            <Button
-                                                type={'primary'}
-                                                onClick={this.onWritePostButtonClick}>
-                                                启动
-                                            </Button>
-                                        }/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'file-text'}
-                                        title={'管理文章'}
-                                        content={<Button>启动</Button>}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>评论管理</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'eye'}
-                                        iconColor={'#1890ff'}
-                                        title={'审核评论'}
-                                        content={<Button type={'primary'}>启动</Button>}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'search'}
-                                        title={'未审核评论'}
-                                        content={12}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'message'}
-                                        title={'管理评论'}
-                                        content={<Button>启动</Button>}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <div>
-                            <div className={'font-size-sm'}>
-                                <Link className={'color-black text-decoration-none'} to={'#'}>留言管理</Link>
-                            </div>
-                            <Row className={'mt-lg'}>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'eye'}
-                                        iconColor={'#1890ff'}
-                                        title={'审核留言'}
-                                        content={<Button type={'primary'}>启动</Button>}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'search'}
-                                        title={'未审核留言'}
-                                        content={12}/>
-                                </Col>
-                                <Col span={8}>
-                                    <AdminIconGadget
-                                        iconType={'message'}
-                                        title={'管理留言'}
-                                        content={<Button>启动</Button>}/>
-                                </Col>
-                            </Row>
-                            <Divider/>
-                        </div>
-
-                        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                        {welcomeBlock}
+                        {todaySummaryBlock}
+                        {hardWorkValueBlock}
+                        {securityValueBlock}
+                        {postManageBlock}
+                        {commentManageBlock}
+                        {messageManageBlock}
                     </Col>
                 </Row>
             </KLayout>
