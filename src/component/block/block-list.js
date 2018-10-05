f3c1e742468c9b95fe4efb0a25f4fb88bccfad1e
John Kindem <461425614@qq.com>
2018-10-05 18:37:27 +0800

    [refactor] 优化组件代码

--- src/component/block/block-list.js
+++ src/component/block/block-list.js
@@ -38,46 +38,64 @@ export class BlockList extends React.Component {
     }
 
     /**
+     * blocks 迭代映射函数
+     * @param {Object} block 块对象
+     * @param {number} key 迭代标识
+     * @returns {*} 渲染结果
+     */
+    blocksMapFunc = (block, key) => {
+        switch (block.type) {
+            case 'post':
+                return (
+                    <div key={key}>
+                        <PostBlock
+                            postKey={block.postKey}
+                            title={block.title}
+                            description={block.description}
+                            date={block.date}
+                            labels={block.labels}/>
+                        <Divider/>
+                    </div>
+                );
+            case 'emotion':
+                return (
+                    <div key={key}>
+                        <EmotionBlock
+                            context={block.context}
+                            date={block.date}/>
+                        <Divider/>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
+
+    /**
+     * 分页状态改变回调
+     * @param {number} page 页数
+     */
+    onPageChange = (page) => {
+        this.props.onPageChange(page);
+    };
+
+    /**
      * 渲染函数
      * @returns {*} 渲染结果
      */
     render() {
+        // 分页div
+        let paginationDiv = (
+            <div className={'text-align-center mt-lg'}>
+                <Pagination size={'small'} total={100} onChange={this.onPageChange}/>
+            </div>
+        );
+
         return (
             <Row className={'w-block-list m-0-auto'}>
                 <Col>
-                    {this.props.blocks.map((block, key) => {
-                        switch (block.type) {
-                            case 'post':
-                                return (
-                                    <div key={key}>
-                                        <PostBlock
-                                            postKey={block.postKey}
-                                            title={block.title}
-                                            description={block.description}
-                                            date={block.date}
-                                            labels={block.labels}/>
-                                        <Divider/>
-                                    </div>
-                                );
-                            case 'emotion':
-                                return (
-                                    <div key={key}>
-                                        <EmotionBlock
-                                            context={block.context}
-                                            date={block.date}/>
-                                        <Divider/>
-                                    </div>
-                                );
-                            default:
-                                return null;
-                        }
-                    })}
-
-                    <div className={'text-align-center mt-lg'}>
-                        <Pagination size={'small'} total={100} onChange={(page) => {
-                            this.props.onPageChange(page);
-                        }}/>
-                    </div>
+                    {this.props.blocks.map(this.blocksMapFunc)}
+                    {paginationDiv}
                 </Col>
             </Row>
         );
