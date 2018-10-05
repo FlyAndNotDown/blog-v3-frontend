8201adf9834600d5eeb3d9bf7914bc30a1847f0e
John Kindem <461425614@qq.com>
2018-10-05 11:31:01 +0800

    [add] 添加归档页面导航栏

--- src/page/archive.js
+++ src/page/archive.js
@@ -4,23 +4,39 @@
  */
 
 import React from 'react';
+import { KLayout } from "../component/tool/k-layout";
+import { Affix } from "antd";
+import { NavBar } from "../component/nav-bar";
 
 /**
  * 归档页面组件
  */
 export class ArchivePage extends React.Component {
 
+    /**
+     * 构造
+     * @param {Object} props 属性
+     */
     constructor(props) {
         super(props);
 
         this.state = {};
     }
 
+    /**
+     * 渲染函数
+     * @returns {*} 渲染结果
+     */
     render() {
         return (
-            <div>
-
-            </div>
+            <KLayout
+                colorMode={KLayout.COLOR_MODE_MAIN}>
+                <KLayout colorMode={KLayout.COLOR_MODE_NONE}>
+                    <Affix offsetTop={0}>
+                        <NavBar active={true}/>
+                    </Affix>
+                </KLayout>
+            </KLayout>
         );
     }
 
