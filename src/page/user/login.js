e37253a6f92e636ed6458da9712d0716947b28ef
John Kindem <461425614@qq.com>
2019-01-28 21:58:29 +0800

    🎉 add some code

--- src/page/user/login.js
+++ src/page/user/login.js
@@ -100,8 +100,23 @@ export class UserLoginPage extends React.Component {
     /**
      * handle called when login button clicked
      */
-    onLoginButtonClick = () => {
+    onLoginButtonClick = async () => {
+        // lock the form
+        this.lock();
+
+        // check the input value at first
+        let { email, password } = this.state;
+        if (!email || !email.match(userRegex.email)) {
+            this.unlock();
+            return message.error('邮箱地址不符合要求');
+        }
+        if (!password || !password.match(userRegex.password)) {
+            this.unlock();
+            return message.error('密码不符合要求');
+        }
+
         // TODO
+        
     };
 
     /**
