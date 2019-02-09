4273a2bb9f5a5fb07e88500a2a595f3720e99724
John Kindem <461425614@qq.com>
2019-02-09 22:59:56 +0800

    🎉 add some code about UI

---
+++ src/component/block/comment-block-list.js
@@ -0,0 +1,38 @@
+/**
+ * /component/block/comment-block-list.js
+ * @author John Kindem
+ * @description source file for component block list component
+ * @version v1.0
+ */
+
+import React from 'react';
+
+/**
+ * CommentBlockList component
+ * @constructor
+ */
+export class CommentBlockList extends React.Component {
+
+    /**
+     * constructor of react component
+     * @param {*} props 
+     */
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    /**
+     * render function
+     * @returns {*} render result
+     */
+    render() {
+        // return render result
+        return (
+            <div>
+            </div>
+        );
+    }
+
+}
