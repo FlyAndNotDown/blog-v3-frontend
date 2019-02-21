b3fe689cfec6245e7bee1db6124f0389d09c8703
John Kindem <461425614@qq.com>
2019-02-21 15:14:06 +0800

    ✨ update configs & settings

--- src/config/option.js
+++ src/config/option.js
@@ -9,34 +9,7 @@ import mainConfig from './main';
 /**
  * 导出选项
  */
-export default mainConfig.devMode ? {
-    postPerPage: 10,
-    labelColors: [
-        '#FFD700',
-        '#FF6A6A',
-        '#C6E2FF',
-        '#E6E6FA',
-        '#C9C9C9',
-        '#98FB98',
-        '#8470FF',
-        '#66CD00',
-        '#48D1CC',
-        '#1C86EE'
-    ],
-    colorPrefebs: [
-        '#FFD700',
-        '#FF6A6A',
-        '#C6E2FF',
-        '#E6E6FA',
-        '#C9C9C9',
-        '#98FB98',
-        '#8470FF',
-        '#66CD00',
-        '#48D1CC',
-        '#1C86EE'
-    ],
-    dateFormat: 'YYYY-MM-DD HH:mm:ss'
-} : {
+export default {
     postPerPage: 10,
     labelColors: [
         '#FFD700',
