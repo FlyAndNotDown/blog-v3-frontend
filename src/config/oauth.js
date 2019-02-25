9dd63675ee924781ece4cac1f5815970f2cc7a9f
John Kindem <461425614@qq.com>
2019-02-25 20:06:00 +0800

    🎉 add a new config about OAuth

---
+++ src/config/oauth.js
@@ -0,0 +1,18 @@
+/**
+ * /config/oauth.js
+ * @author John Kindem
+ * @version v1.0
+ */
+
+export default {
+    // qq login config
+    qq: {
+        appId: '1105493157',
+        appKey: '3WcztJ3aEu9vn3oB'
+    },
+    // github login config
+    github: {
+        clientId: '2b18fc8f7305f2e73416',
+        clientSecret: 'eee6029f6f6f5873cc3fc8fcf9ebdb86ef375349'
+    }
+};
